199d961c45ab1474f481eccdb601147a998cbd7b
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-10-03 21:51:29 +0530

    Add files via upload

---
+++ solution2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class solution1{
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+         String name= scan.nextLine();
+        name+=scan.nextLine();
+         scan.close();
+
+        System.out.println("Enter your name"+name); 
+
+    }
+
+}
